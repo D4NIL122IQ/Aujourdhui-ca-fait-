2dc759f15cd8f66571eae5923066e7bb2b06982b
Danil gdj <danilguidjou7@gmail.com>
2024-06-12 02:21:56 +0100

    javaScript finish

--- function.js
+++ function.js
@@ -9,24 +9,24 @@ function verification() {
     if (year.value == ""|| month.value == "" || day.value == "" || year.value > todayDate.getFullYear()) {
         return false;
     } else { 
-
-        console.log("zz")
-        if (month.value == 2 && day.value<= 29) {
-            calcule()
-        }
-        if ( [1,3,5,7,8,10,12].includes(parseInt(month.value)) && day.value <= 31){
-            calcule()
-        }
-        if ( [4,6,9,11].includes(month.value) && day.value<= 30){
-            calcule
-            
-        }
+        if( (year.value == todayDate.getFullYear() && month.value > todayDate.getMonth() +1 ) 
+            || (year.value == todayDate.getFullYear() && ( month.value == todayDate.getMonth() +1 )&& day.value > todayDate.getDate()) ){
+                return false
+        }else{
+            if ( (month.value == 2 && day.value<= 29) 
+                || ( [1,3,5,7,8,10,12].includes(parseInt(month.value)) && day.value <= 31) 
+                || ( [4,6,9,11].includes(parseInt(month.value)) && day.value<= 30)) {
+                    return true
+            }else{
+                return false;
+            }
+        }   
     }
 }
 
-// on calcule le nombre d'annee mois et jour qui nous separe de cette date 
+// on calcule le nombre d'annee, mois et jour qui nous separe de cette date 
 function calcule() {
-    document.querySelector(".annee").innerHTML =todayDate.getFullYear() - year.value;
+    document.querySelector(".annee").innerHTML = todayDate.getFullYear() - year.value;
     if (month.value > todayDate.getMonth() + 1) {
         document.querySelector(".annee").innerHTML -= "1"
         document.querySelector(".mois").innerHTML = 13 +(todayDate.getMonth() - month.value  )
@@ -34,8 +34,8 @@ function calcule() {
         document.querySelector(".mois").innerHTML =  (todayDate.getMonth() + 1) - month.value 
     }
     if(day.value > todayDate.getDate()){
-        document.querySelector(".mois").innerHTML -= "1"
-        document.querySelector(".jour").innerHTML = 31 + (todayDate.getDate() - day.value);
+        document.querySelector(".mois").innerHTML = parseInt(document.querySelector(".mois").innerHTML) + 1
+        document.querySelector(".jour").innerHTML = - (todayDate.getDate() - day.value);
     }else{
         document.querySelector(".jour").innerHTML = todayDate.getDate() - day.value
     }
@@ -44,7 +44,9 @@ function calcule() {
 
 
 document.querySelector("input[type='submit']").addEventListener("click" , ()=>{
-    calcule()
-        
+    if (verification()) {
+        calcule();
+    } else {
+        document.querySelector(".result").innerHTML = "erreur"
+    }
 });
-
